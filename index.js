7b409ef63c02061edab886cc243d4a7eeec17809
Leo <leo.haanpaa@edu.tampere.fi>
2024-08-07 22:44:41 +0300

    Finished the project

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 //Random number generator program
 
-const myButton = document.getElementById("myButton");
-const myLabel = document.getElementById("myLabel");
-const min = 1;                                        //Defines the minimum number
-const max = 6;                                        //Defines the maximum number
-let randomNum;
+const myButton = document.getElementById("myButton");  //saves the assigned variable data
+const myLabel = document.getElementById("myLabel");    //saves the assigned variable data
+const min = 1;                                         //Defines the minimum number
+const max = 6;                                         //Defines the maximum number
+let randomNum;                                         //Declares the random number
 
 myButton.onclick = function(){
-    randomNum = Math.floor(Math.random() * max) + min; //generates a random number between 1 and 6
+    randomNum = Math.floor(Math.random() * max) + min; //Generates a random number between 1 and 6
     myLabel.textContent = randomNum;
 }
-//console.log(randomNum);                                //Displays the random number
