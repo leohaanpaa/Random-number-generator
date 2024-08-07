c57b0c0864c411f211a60c4821ddaf9e1616fb38
Leo <leo.haanpaa@edu.tampere.fi>
2024-08-07 20:35:20 +0300

    Baseline done

---
+++ index.js
@@ -0,0 +1,7 @@
+//Random number generator program
+
+const min = 50;                                        //Defines the minimum number
+const max = 100;
+let randomNum = math.floor(Math.random() * (max - min)) + min; //gives a random number between  and 
+
+console.log(randomNum);                            //Displays the random number
